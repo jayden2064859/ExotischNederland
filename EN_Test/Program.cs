cab9785e3b633c91ecd85af88e56139400ae6233
M.J. Koeman <2406365koeman@zuyd.nl>
2025-01-17 09:59:23 +0100

    Update Program.cs

--- EN_Test/Program.cs
+++ EN_Test/Program.cs
@@ -3,7 +3,7 @@
  Database toevoegen waar de registraties in komen te staan en om accountgegevens op te slaan zodat gegevens gechecked worden (Medewerker menu heeft optie om database in te zien)
  Meer serious games (Fun facts/Educatieve sectie, Raad het dier, waar of onwaar etc)
  Automatische Locatiebepaling?
- Bij statestieken een top 5/top 10 meest geregistreerde dieren maken, in plaats van maar 1
+ Bij statistieken een top 5/top 10 meest geregistreerde dieren maken, in plaats van maar 1
  Medewerkers moeten een optie in hun hoofdmenu hebben om de nieuwe registraties goed te keuren en aanpassen/verwijderen indien nodig
  ^ Voeg een extra waarde toe bij de registraties; 'Verified' en 'pending' voor de registraties die gechecked moeten worden door medewerkers
 */
